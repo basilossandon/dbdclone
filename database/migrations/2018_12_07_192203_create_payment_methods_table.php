75b44bb0d687b8e47290b438cde2ab0422edbcae
Arefer <kevin.arevalo@usach.cl>
2018-12-12 15:48:10 -0300

    Proyecto laravel inicializado

---
+++ database/migrations/2018_12_07_192203_create_payment_methods_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentMethodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_methods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('payment_method_name');
+            $table->string('payment_method_type');
+            $table->integer('account_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_methods');
+    }
+}
---
+++ database/migrations/2018_12_07_192240_create_reservations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamp('reservation_date');
+            $table->string('reservation_ip');
+            $table->string('reservation_detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
