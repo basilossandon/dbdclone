4b668929a5c6861d1244be03d67455e7c12c5a4f
Arefer <kevin.arevalo@usach.cl>
2018-12-13 20:46:05 +0000

    Creada migracion tabla pivote reserva-habitacion

---
+++ database/migrations/2018_12_13_204442_create_reservation_room_pivot_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class App\ extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservation_room', function (Blueprint $table) {
+            $table->integer('reservation_id')->unsigned()->index();
+            $table->foreign('reservation_id')->references('id')->on('reservations')->onDelete('cascade');
+            $table->integer('room_id')->unsigned()->index();
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
+            $table->primary(['reservation_id', 'room_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reservation_room');
+    }
+}
