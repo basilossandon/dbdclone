7915195f9b95a35e8ec5dabc6fa75fce0c82aa54
vagrant <diego.galleguillos@usach.cl>
2018-12-13 21:08:29 +0000

    Arregladas las tablas pivote para migracion de nuevo

--- database/migrations/2018_12_13_204442_create_reservation_room_pivot_table.php
+++ database/migrations/2018_12_13_204442_create_reservation_room_pivot_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRReservationRoomPivotTable extends Migration
+class CreateReservationRoomPivotTable extends Migration
 {
     /**
      * Run the migrations.
