7c2c8ceee0ad53121ec5dea1d93c960a5980e69e
vagrant <diego.galleguillos@usach.cl>
2018-12-13 20:42:55 +0000

    creada tabla pivote pasaje-reserva

---
+++ database/migrations/2018_12_13_204206_create_passage_reservation_pivot_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class App\ extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passage_reservation', function (Blueprint $table) {
+            $table->integer('passage_id')->unsigned()->index();
+            $table->foreign('passage_id')->references('id')->on('passages')->onDelete('cascade');
+            $table->integer('reservation_id')->unsigned()->index();
+            $table->foreign('reservation_id')->references('id')->on('reservations')->onDelete('cascade');
+            $table->primary(['passage_id', 'reservation_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('passage_reservation');
+    }
+}
