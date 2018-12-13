62b31536ed506304a0b6fcb8dd57589280efe64b
vagrant <diego.galleguillos@usach.cl>
2018-12-13 21:06:29 +0000

    Arregladas las tablas pivote para migracion

--- database/migrations/2018_12_13_204127_create_payment_method_receipt_pivot_table.php
+++ database/migrations/2018_12_13_204127_create_payment_method_receipt_pivot_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class App\ extends Migration
+class CreatePaymentMethodReceiptPivotTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2018_12_13_204206_create_passage_reservation_pivot_table.php
+++ database/migrations/2018_12_13_204206_create_passage_reservation_pivot_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class App\ extends Migration
+class CreatePassageReservationPivotTable extends Migration
 {
     /**
      * Run the migrations.
