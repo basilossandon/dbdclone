dc65f4aa10f2fa1c80936aad47df0302cc77b010
vagrant <vagrant@homestead>
2018-12-15 20:27:53 +0000

    Auth agregado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>LATAM Airlines</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    NICOLÁS
+                    LATAM Airlines
                 </div>
 
                 <div class="links">
