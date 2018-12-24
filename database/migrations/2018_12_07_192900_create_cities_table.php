9c3fbfe5b120ef8ef6a4a5ad87d6b2b11c2fe15d
Basil Ossandon <basilossandon@gmail.com>
2018-12-24 03:17:34 +0000

    12 Paises seleccionados

--- database/migrations/2018_12_07_192900_create_cities_table.php
+++ database/migrations/2018_12_07_192900_create_cities_table.php
@@ -16,7 +16,7 @@ class CreateCitiesTable extends Migration
         Schema::create('cities', function (Blueprint $table) {
             $table->increments('id');
             $table->string('city_name');
-            $table->string('city_code');
+            $table->string('country_code');
             $table->timestamps();
             $table->integer('country_id')->unsigned()->index();
             $table->foreign('country_id')->references('id')->on('countries')->onDelete('cascade');
