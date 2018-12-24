2141367e44b4161be81a41fdd11c2618318d4ee1
Arefer <kevin.arevalo@usach.cl>
2018-12-24 17:20:14 +0000

    Eliminada una relacion incorrecta en Airport

--- app/Airport.php
+++ app/Airport.php
@@ -6,14 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Airport extends Model
 {
-	protected $primaryKey = 'id';
+		protected $primaryKey = 'id';
     public function flightsDeparture(){
       return $this->HasMany(App\Flight::class, 'departure_airport_id');
     }
     public function flightsArrival(){
       return $this->HasMany(App\Flight::class, 'arrival_airport_id');
     }
-		public function seats(){
-			return $this->belongsToMany(App\Seat::class);
-		}
 }
