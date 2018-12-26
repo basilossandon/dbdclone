464e6a2168324ec391049a13591da23863872080
Arefer <kevin.arevalo@usach.cl>
2018-12-26 20:53:41 +0000

    Eliminada la tabla pivote ticket_reservation

--- database/seeds/ReservationVehicleTableSeeder.php
+++ database/seeds/ReservationVehicleTableSeeder.php
@@ -14,14 +14,14 @@ class ReservationVehicleTableSeeder extends Seeder
         $reservations = App\Reservation::all();
         $faker = Faker\Factory::create('en_US');
 
-      App\Vehicle::all()->each(function ($vehicle) use ($reservations, $faker){
-        $idsArray = $reservations->random(rand(1,3))->pluck('id')->toArray();
-        foreach($idsArray as $id){
-          $vehicle->reservations()->attach($id, [
-            'vehicle_reservation_lease' => $faker->dateTimeBetween($min = '-1 years', $endDate = 'now'),
-            'vehicle_reservation_return' => $faker->dateTimeBetween($min = '+ 1 days', $endDate = '+ 30 days'),
-          ]);
-        }
-      });
+        App\Vehicle::all()->each(function ($vehicle) use ($reservations, $faker){
+          $idsArray = $reservations->random(rand(1,3))->pluck('id')->toArray();
+          foreach($idsArray as $id){
+            $vehicle->reservations()->attach($id, [
+              'vehicle_reservation_lease' => $faker->dateTimeBetween($min = '-1 years', $endDate = 'now'),
+              'vehicle_reservation_return' => $faker->dateTimeBetween($min = '+ 1 days', $endDate = '+ 30 days'),
+            ]);
+          }
+        });
     }
 }
