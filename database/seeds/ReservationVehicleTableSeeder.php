271108db2825ac5867bb78dc9c7607c4f9a0f30b
vagrant <diego.galleguillos@usach.cl>
2018-12-26 04:48:37 +0000

    cambios ReservationVehicleTableSeeder

--- database/seeds/ReservationVehicleTableSeeder.php
+++ database/seeds/ReservationVehicleTableSeeder.php
@@ -12,11 +12,16 @@ class ReservationVehicleTableSeeder extends Seeder
     public function run()
     {
         $reservations = App\Reservation::all();
+        $faker = Faker\Factory::create('en_US');
 
-      App\Vehicle::all()->each(function ($vehicle) use ($reservations){
-        $vehicle->reservations()->attach(
-          $reservations->random(rand(1,3))->pluck('id')->toArray()
-        );
+      App\Vehicle::all()->each(function ($vehicle) use ($reservations, $faker){
+        $idsArray = $reservations->random(rand(1,3))->pluck('id')->toArray();
+        foreach($idsArray as $id){
+          $vehicle->reservations()->attach($id, [
+            'vehicle_reservation_lease' => $faker->dateTimeBetween($min = '-1 years', $endDate = 'now'),
+            'vehicle_reservation_return' => $faker->dateTimeBetween($min = '+ 1 days', $endDate = '+ 30 days'),
+          ]);
+        }
       });
     }
 }
