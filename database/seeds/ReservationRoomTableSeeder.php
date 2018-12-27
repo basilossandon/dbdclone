2682fe98b66439eb0b981824673502ac990aabb6
Arefer <kevin.arevalo@usach.cl>
2018-12-27 15:12:19 -0300

    Corregido seeder ReservationRoom

--- database/seeds/ReservationRoomTableSeeder.php
+++ database/seeds/ReservationRoomTableSeeder.php
@@ -15,12 +15,14 @@ class ReservationRoomTableSeeder extends Seeder
        $reservations = App\Reservation::all();
        $faker = Faker\Factory::create('en_US');
 
-       App\Vehicle::all()->each(function ($vehicle) use ($reservations, $faker){
+       App\Room::all()->each(function ($room) use ($reservations, $faker){
          $idsArray = $reservations->random(rand(1,3))->pluck('id')->toArray();
          foreach($idsArray as $id){
-           $vehicle->reservations()->attach($id, [
-             'reservation_room_lease' => $faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now'),
-             'reservation_room_return' => $faker->dateTimeBetween($startDate = '+ 1 days', $endDate = '+ 30 days'),
+           $room->reservations()->attach($id, [
+             'reservation_room_lease' => $faker->dateTimeBetween($startDate = '-1 years',
+             $endDate = 'now'),
+             'reservation_room_return' => $faker->dateTimeBetween($startDate = '+ 1 days',
+             $endDate = '+ 30 days'),
            ]);
          }
        });
