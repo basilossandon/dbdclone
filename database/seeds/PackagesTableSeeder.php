cd02bd471b55f96727094ee703ab132c1381cede
Arefer <kevin.arevalo@usach.cl>
2018-12-27 03:55:09 -0300

    Modificado el seeder de Package

--- database/seeds/PackagesTableSeeder.php
+++ database/seeds/PackagesTableSeeder.php
@@ -11,6 +11,18 @@ class PackagesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Package::class, 10)->create();
+        $packages = factory(App\Package::class, 10)->create();
+        $packages->each(function ($package){
+            $roomsId = App\Room::all()->random(rand(1,3))->pluck('id')->toArray();
+            foreach($roomsId as $id){
+                $package->rooms()->attach($id);
+            }
+            // Crear un ticket "no comprado" (passenger_id = null, reservation_id = null,
+            // seat_id = null, seat_letter = null)
+            $ticket = new App\Ticket;
+            $ticket->package_id = $package->id;
+            $ticket->seat_id = App\Seat::all()->random()->id;
+            $ticket->save();
+        });
     }
 }
