839dc03c9100ad0cbfb9dffb326ff171a8a57646
Basil Ossandon Hostens <basilossandon@gmail.com>
2018-12-28 04:51:39 +0000

    Carousel arreglado un poquito

--- resources/views/carousel/carousel.blade.php
+++ resources/views/carousel/carousel.blade.php
@@ -1,48 +1,39 @@
-<div id="carousel-example-generic">
-    <div class="carousel slide">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        </ol>
+<link rel="stylesheet" href="/css/bootstrap.min.css">
+<script src="/js/popper.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/jquery-3.3.1.slim.js"></script>
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-            <div class="item active">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
                 <img src="/images/img1.jpg" alt="Chile" />
                 <div class="carousel-caption">
                     <h1>Recently ranked the #1
 place to travel in the world.</h1>
-                    <p>Take me to Chile!</p>
-                </div>
-            </div>
-
-            <div class="item">
+                    <p>Take me to Chile!</p>    </div>
+    <div class="carousel-item">
                 <img src="/images/img2.jpg" alt="Travel" />
                 <div class="carousel-caption">
                     <h1>Travel to more than one place</h1>
-                    <p>Try our Multi-City option</p>
-                </div>
-            </div>
-
-            <div class="item">
+                    <p>Try our Multi-City option</p>    </div>
+    <div class="carousel-item">
                 <img src="/images/img3.jpg" alt="Peru" />
                 <div class="carousel-caption">
                     <h1>Ancient civilizations
 and adventure-filled days</h1>
-                    <p>Peru is waiting for you.</p>
-                </div>
-            </div>
+                    <p>Peru is waiting for you.</p>    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
-            <!-- Controls -->
-            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                <span class="icon-prev"></span>
-            </a>
-            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                <span class="icon-next"></span>
-            </a>
-        
-    </div>
-</div>
-
