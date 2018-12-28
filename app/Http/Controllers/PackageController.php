2449a26afedca4c86d60bc65139c37a51621be9a
Arefer <kevin.arevalo@usach.cl>
2018-12-27 23:56:39 -0300

    Creada la funcion showDetail, que muestra los servicios de un paquete

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 use App\Package;
+use App\Ticket;
+use App\FlightUserInfo;
+use App\Airport;
 use Illuminate\Http\Request;
+use \Illuminate\Support\Collection;
 
 class PackageController extends Controller
 {
@@ -80,6 +84,43 @@ class PackageController extends Controller
     public function destroy($id)
     {
         $package = Package::find($id);
-        $package->delete();       
+        $package->delete();
+    }
+
+    /**
+    * Get all the services asociated to a package
+    * @param int $packageId
+    *
+    */
+    public function showDetail($packageId){
+        $package = Package::find($packageId);
+        $tickets = $package->tickets()->get();
+        $flightsInfo = Collection::make();
+        foreach($tickets as $ticket){
+            $flightInfo = new FlightUserInfo;
+            $departureAirportId = $ticket->flight->departure_airport_id;
+            $arrivalAirportId = $ticket->flight->arrival_airport_id;
+
+            $departureAirport = Airport::find($departureAirportId);
+            $arrivalAirport = Airport::find($arrivalAirportId);
+
+            $origin = $departureAirport->city->city_name;
+            $destiny = $arrivalAirport->city->city_name;
+            $seat_type = $ticket->seat->seat_type;
+            $flightsInfo->push(new class($origin, $destiny, $seat_type){
+                public $origin;
+                public $destiny;
+                public $seat_type;
+                public function __construct($origin, $destiny, $seat_type){
+                  $this->origin = $origin;
+                  $this->destiny = $destiny;
+                  $this->seat_type = $seat_type;
+                }
+            });
+        }
+        $rooms = $package->rooms()->get();
+        // Un paquete tiene un solo vehiculo
+        $vehicle = $package->vehicle()->first();
+        return collect([$flightsInfo, $rooms, $vehicle]);
     }
 }
