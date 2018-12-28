85e92c015ce2401af156adb83ab2441a8c5a6be9
Basil Ossandon Hostens <basilossandon@gmail.com>
2018-12-28 01:28:05 +0000

    Register controller

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,80 +6,50 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return Register::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function createOrEdit()
     {
-        //
+        return view('registers');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function storeOrUpdate(Request $request)
     {
-        //
+        $auxRegister = Register::find($request->id);
+        if($auxRegister == null){
+            $register = new Register();
+            $register->updateOrCreate([
+                'user_id' => $request->user_id,
+                'modified_table_name' => $request->modified_table_name,
+                'modification' => $request->modification
+            ],[]);
+        }
+        else{
+            $register = new Register();
+            $register->updateOrCreate([
+                'id' => $request->id,
+            ], 
+                ['user_id' => $request->user_id,
+                'modified_table_name' => $request->modified_table_name,
+                'modification' => $request->modification
+            ]);   
+        }
+        return Register::all();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         return Register::find($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        $receipt = Register::find($id);
-        $receipt->delete();
+        $register = Register::find($id);
+        $register->delete();
+        return Register::all();
     }
-}
+
