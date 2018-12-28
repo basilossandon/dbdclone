bbb45e79372078fedfa945c509a24825754822f7
Basil Ossandon Hostens <basilossandon@gmail.com>
2018-12-28 07:29:06 +0000

    Hotel funciones basicas agregadas

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -8,81 +8,55 @@ use \Illuminate\Support\Collection;
 use Carbon\Carbon;
 class HotelController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        return Hotel::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function createOrEdit()
     {
-        //
+        return view('hotels');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function storeOrUpdate(Request $request)
     {
-        //
+        $auxhotel = Hotel::find($request->id);
+        if($auxhotel == null){
+            $hotel = new Hotel();
+            $hotel->updateOrCreate([
+                'hotel_name' => $request->hotel_name,
+                'hotel_address' => $request->hotel_address,
+                'hotel_stars' => $request->hotel_stars,
+                'city_id' => $request->city_id
+            ],[]);
+        }
+        else{
+            $hotel = new Hotel();
+            $hotel->updateOrCreate([
+                'id' => $request->id,
+            ], 
+                ['hotel_name' => $request->hotel_name,
+                'hotel_address' => $request->hotel_address,
+                'hotel_stars' => $request->hotel_stars,
+                'city_id' => $request->city_id
+            ]);   
+        }
+        return Hotel::all();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        return Hotel::find($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $hotel = Hotel::find($id);
+        $hotel->delete();
+        return Hotel::all();
     }
+}
+
 
     public function showRooms($hotel_id){
         $hotel = Hotel::find($hotel_id);
