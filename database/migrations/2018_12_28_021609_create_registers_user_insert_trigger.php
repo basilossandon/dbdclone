e2a7a73245c213b8eb0ea6a1273de078b0f161eb
diego-gavel <diego.galleguillos@usach.cl>
2018-12-28 15:06:20 -0300

    cambio de nombre de trigger para mayor representatividad

--- database/migrations/2018_12_28_021609_create_registers_user_insert_trigger.php
+++ database/migrations/2018_12_28_021609_create_registers_user_insert_trigger.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRegistersInsertTrigger extends Migration
+class CreateRegistersUserInsertTrigger extends Migration
 {
    public function up()
    {
