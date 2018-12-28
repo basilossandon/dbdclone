db39a479945da0cf620040ac919883a1e6566888
Basil Ossandon Hostens <basilossandon@gmail.com>
2018-12-28 01:22:47 +0000

    Flight controller

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -7,91 +7,59 @@ use Illuminate\Http\Request;
 
 class FlightController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return Flight::all();
     }
 
-    //public function filterByUser
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function createOrEdit()
     {
-        //
+        return view('flights');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function storeOrUpdate(Request $request)
     {
-        Flight::create($request->all());
+        $auxflight = Flight::find($request->id);
+        if($auxflight == null){
+            $flight = new Flight();
+            $flight->updateOrCreate([
+                'flight_departure' => $request->flight_departure,
+                'flight_arrival' => $request->flight_arrival,
+                'flight_assigned_plane' => $request->flight_assigned_plane,
+                'flight_distance' => $request->flight_distance,
+                'flight_capacity' => $request->flight_capacity,
+                'flight_code' => $request->flight_code,
+                'departure_airport_id' => $request->departure_airport_id,
+                'arrival_airport_id' => $request->arrival_airport_id
+            ],[]);
+        }
+        else{
+            $flight = new Flight();
+            $flight->updateOrCreate([
+                'id' => $request->id,
+            ], 
+                ['flight_departure' => $request->flight_departure,
+                'flight_arrival' => $request->flight_arrival,
+                'flight_assigned_plane' => $request->flight_assigned_plane,
+                'flight_distance' => $request->flight_distance,
+                'flight_capacity' => $request->flight_capacity,
+                'flight_code' => $request->flight_code,
+                'departure_airport_id' => $request->departure_airport_id,
+                'arrival_airport_id' => $request->arrival_airport_id
+            ]);   
+        }
+        return Flight::all();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         return Flight::find($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $flight = Flight::find($id);
-        $flight->flight_code = $request->input('flight_code');
-        $flight->flight_capacity = $request->input('flight_capacity');
-        $flight->flight_distance = $request->input('flight_distance');
-        $flight->flight_assigned_plane = $request->input('flight_assigned_plane');
-        $flight->flight_departure = $request->input('flight_departure');
-        $flight->flight_arrival = $request->input('flight_arrival');
-        $flight->departure_airport_id = $request->input('departure_airport_id');
-        $flight->arrival_airport_id = $request->input('arrival_airport_id');
-        $flight->save();
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $flight = Flight::find($id);
         $flight->delete();
+        return Flight::all();
     }
 }
