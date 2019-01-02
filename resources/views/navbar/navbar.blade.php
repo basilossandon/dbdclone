d62014fdad7aae00bdf355b5749c9d5e7a17b006
Basil Ossandon Hostens <basilossandon@gmail.com>
2019-01-02 02:36:52 +0000

    Logo funciona en cualquier blade

--- resources/views/navbar/navbar.blade.php
+++ resources/views/navbar/navbar.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg">
 
-
-<img src=./images/logo.png class="logo"> 
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+<a href="{!! URL::to('/') !!}">
+   <img src="/images/logo.png" />
+</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
 <span class="navbar-toggler-icon"></span>
 </button>
 <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
