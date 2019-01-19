330fb0f391f108f1702ba67d159402bfe6259700
Basil Ossandon Hostens <basilossandon@gmail.com>
2019-01-19 22:21:45 +0000

    Layout clean

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,4 @@
 <!DOCTYPE html>
-    {{-- searchbox inside dropdown --}}
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
-    {{-- DatePicker --}}
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="https://unpkg.com/gijgo@1.9.11/js/gijgo.min.js" type="text/javascript"></script>
-    <link href="https://unpkg.com/gijgo@1.9.11/css/gijgo.min.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="/css/reserve.css">
-
-
-@section('scripts')
-    {{-- searchbox inside dropdown --}}
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
-    <script>$('.select2').select2();</script>
-@endsection
 <html>
     <head>
         <meta name="viewport" content="width=device-width, user-scalable=no initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
--- resources/views/reserve.blade.php
+++ resources/views/reserve.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.app')
+{{-- searchbox inside dropdown --}}
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+<script>$('.select2').select2();</script>
+{{-- DatePicker --}}
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://unpkg.com/gijgo@1.9.11/js/gijgo.min.js" type="text/javascript"></script>
+<link href="https://unpkg.com/gijgo@1.9.11/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+<script src="/js/buttons.js"></script>
+
 @section('title', 'Reserva tu vuelo')
 @section('header')
-
 @section('content')
 
-<script src="/js/buttons.js"></script>
 <div class="reservationFormWrapper">
 <div class="reserveForm">
   <form action="/reserve/choose_flights" method="POST" >
