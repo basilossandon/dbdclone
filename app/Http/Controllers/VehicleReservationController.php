023eb78589648c1e19c1d07e2682d583f951f56d
diego-gavel <diego.galleguillos@usach.cl>
2019-01-25 05:26:35 -0300

    Modificaciones al controlador de reserva de vehiculos

--- app/Http/Controllers/VehicleReservationController.php
+++ app/Http/Controllers/VehicleReservationController.php
@@ -9,10 +9,16 @@ use Carbon\Carbon;
 use App\Vehicle;
 use Cart;
 use App\Receipt;
+use Auth;
+use App\Reservation;
+use App\PaymentMethod;
 
 class VehicleReservationController extends Controller
 {
     public function searchVehicles(){
+        $user_id = Auth::id();
+        Cart::session($user_id);
+        Cart::clear();
         $cities = City::all();
         $vehicle_types = VehicleController::getVehicleTypes();
         return view('reserveVehicle', compact('cities', 'vehicle_types'));
@@ -37,7 +43,7 @@ class VehicleReservationController extends Controller
         ));
 
         $vehicles = Vehicle::all();
-        $available_vehicles = $vehicles->filter(function ($vehicles) use($city_id, $pickup_date,$dropoff_date) {
+        $available_vehicles = $vehicles->filter(function ($vehicles) use($city_id, $pickup_date,$dropoff_date, $vehicle_type) {
 
           $thereArentReservations = $vehicles->reservations->every(function ($reservation) use ($pickup_date, $dropoff_date){
               $reservation_lease = Carbon::parse($reservation->pivot->vehicle_reservation_lease);
@@ -46,7 +52,7 @@ class VehicleReservationController extends Controller
                       !($reservation_lease->between($pickup_date, $dropoff_date)) &&
                       !($reservation_return->between($pickup_date, $dropoff_date)));
               });
-          return ($vehicles->city_id == $city_id && $thereArentReservations);
+          return ($vehicles->city_id == $city_id && $thereArentReservations && $vehicles->vehicle_type == $vehicle_type);
         });
         return view('chooseVehicles', compact('available_vehicles'));
     }
@@ -55,12 +61,55 @@ class VehicleReservationController extends Controller
       $user_id = Auth::id();
       Cart::session($user_id);
 
-      $pickup_date = Cart::get(0)->attributes['pickup_date']
-      $dropoff_date = Cart::get(0)->attributes['dropoff_date']
+      $pickup_date = Cart::get(0)->attributes[0]['pickup_date'];
+      $dropoff_date = Cart::get(0)->attributes[0]['dropoff_date'];
 
       $vehicle_id = $id;
 
-      // Create receipt
+      // Create reservation
+      $reservation = new Reservation;
+      $reservation->reservation_ip = "test";
+      $reservation->reservation_date = date('Y-m-d H:i:s');
+      $reservation->save();
+      // Create a receipt and link it to the logged user
+      $receipt = new Receipt;
+      $receipt->receipt_date = date('Y-m-d H:i:s');
+      $receipt->receipt_type = "boleta";
+      $receipt->receipt_ammount = Vehicle::find($vehicle_id)->vehicle_price;
+      $receipt->user_id = $user_id;
+      $receipt->reservation_id = $reservation->id;
+      $receipt->save();
+
+      $reservation->vehicles()->attach($vehicle_id, ['vehicle_reservation_lease' => $pickup_date,
+                                                     'vehicle_reservation_return' => $dropoff_date]);
+
+       Cart:: update(0, array(
+           'price' => Vehicle::find($vehicle_id)->vehicle_price,
+           'id' => $receipt->id
+         ));
+
+      return redirect('/reserve/vehicles/pay');
     }
 
+    public function pay(){
+        $user_id = Auth::user()->id;; // Logged user
+        Cart::session($user_id);
+        $total = Cart::getContent()->first()->price;
+        return view('payVehicle', compact('total'));
+    }
+
+    public function storePayment(Request $request){
+        $user_id = Auth::user()->id;; // Logged user
+        Cart::session($user_id);
+        $payment_method = new PaymentMethod();
+        $payment_method->card_owner = $request['cc_owner'];
+        $payment_method->card_number = $request['cc_number'];
+        $payment_method->card_expiration_date = $request['cc_exp_mo'].'-'.$request['cc_exp_yr'];
+        $payment_method->card_security_code = $request['cc_security_code'];
+        $payment_method->save();
+        $receipt = Receipt::find(Cart::getContent()->first()->id);
+        $receipt->payment_method_id = $payment_method->id;
+        $receipt->save();
+        return redirect('/');
+    }
 }
